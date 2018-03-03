0559dd49b116960aa2b0104c430f86673aa9ab4e
laloolea <laloolea18@gmail.com>
2018-03-03 10:38:40 -0700

    Agregue otro saludo[omar]

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main () {
+
+    cout <<"Hola mundo"<<endl;
+
+    cout <<"Hola locos"<<endl;
+    system("pause");
+    return 0;
+
+}
